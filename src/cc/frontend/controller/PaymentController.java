48d9b07136054c88ed54ded0f62f23795d69f5f8
sonnh4 <sonnh4@sonnh4-ThinkPad-T450>
2016-01-14 00:30:57 +0700

    Done HTML

--- src/cc/frontend/controller/PaymentController.java
+++ src/cc/frontend/controller/PaymentController.java
@@ -5,17 +5,20 @@
 package cc.frontend.controller;
 
 import cc.frontend.callapi.APISale;
+import cc.frontend.common.BusinessProcess;
 import cc.frontend.common.TGRConfig;
 import com.google.gson.Gson;
 import cc.frontend.common.Utils;
 import cc.frontend.entity.Bank;
+import cc.frontend.entity.Item;
 import cc.frontend.entity.OrderCreateResp;
 import cc.frontend.entity.OrderDetail;
-import cc.frontend.entity.ResponseBank;
 import hapax.TemplateDataDictionary;
 import hapax.TemplateDictionary;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -63,14 +66,50 @@ public class PaymentController extends HttpServlet {
         TemplateDataDictionary myDic = TemplateDictionary.create();
         String mainContent = "";
         if (pathInfo.equals("/banthe") || pathInfo.equals("/naptiengame")) {
-            if (req.getParameter("t") != null) {
-                String totalAmount = Utils.formatNumber(Integer.parseInt(req.getParameter("t")));
-                myDic.setVariable("total_amount", totalAmount);
+            if(this.checkURL(req, pathInfo) == 1){
+                if (req.getParameter("t") != null) {
+                    String totalAmount = Utils.formatNumber(Integer.parseInt(req.getParameter("t")));
+                    myDic.setVariable("total_amount", totalAmount);
+                }
+                myDic.setVariable("list_bank", this.renderListBank());
+                mainContent = Utils.renderTemplate("Template/payment.html", myDic);
             }
-            myDic.setVariable("list_bank", this.renderListBank());
-            mainContent = Utils.renderTemplate("Template/payment.html", myDic);
+            else{
+                mainContent = Utils.render404Page(myDic);
+            }
         } else if (pathInfo.equals("/hoanthanh")) {
-            mainContent = Utils.renderTemplate("Template/payment_finish.html", myDic);
+            if(this.checkURL(req, pathInfo) == 1){
+                String transId = req.getParameter("transid");
+                String amount = req.getParameter("amount");
+                String rescode = req.getParameter("rescode");
+                String type = req.getParameter("type");
+                String status = req.getParameter("status");
+                
+                if(type.equals("zx")){
+                    myDic.setVariable("title", "Nạp tiền ZingXu");
+                }
+                else{
+                    myDic.setVariable("title", "Mua mã thẻ");
+                }
+                
+                String info = "<strong style=\"color:red\">Giao dịch thất bại</strong>";
+                if(rescode.equals("1") && status.equals("1")){
+                    info = "<strong style=\"color:green\">Giao dịch thành công</strong>";
+                    if(type.equals("card")){
+                        myDic.showSection("CARD");
+                    }
+                }
+                
+                myDic.setVariable("status", info);
+                myDic.setVariable("order_no", transId);
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                myDic.setVariable("date_trans", dateFormat.format(Calendar.getInstance().getTime()));
+                myDic.setVariable("total_amount", Utils.formatNumber(Integer.parseInt(amount)));
+                mainContent = Utils.renderTemplate("Template/payment_finish.html", myDic);
+            }
+            else{
+                mainContent = Utils.render404Page(myDic);
+            }
         } else {
             mainContent = Utils.render404Page(myDic);
         }
@@ -94,10 +133,35 @@ public class PaymentController extends HttpServlet {
             }
         }
         else if(pathInfo.equals("/banthe")){
-            
+            Map<String, String[]> mapParam = req.getParameterMap();
+            int cardIndex = 0;
+            String cardIdKey = "id0";
+            String param = "";
+            while(mapParam.get(cardIdKey) != null){
+                String quantityKey = "q" + cardIndex;
+                String id = mapParam.get(cardIdKey)[0];
+                String quantity = mapParam.get(quantityKey)[0];
+                param += String.format("%s=%s&%s=%s&", cardIdKey, id, quantityKey, quantity);
+                cardIndex++;
+                cardIdKey = "id" + cardIndex;
+            }
+            String totalAmount = req.getParameter("t");
+            String reqSig = req.getParameter("sig");
+            String sig = Utils.encryptMD5(param + "|" + totalAmount + "|" + TGRConfig.gApiCheapCard.getSecret());
+            if(sig.equals(reqSig)){
+                result = 1;
+            }
         }
         else if(pathInfo.equals("/hoanthanh")){
-            
+            String transId = req.getParameter("transid");
+            String amount = req.getParameter("amount");
+            String rescode = req.getParameter("rescode");
+            String type = req.getParameter("type");
+            String sig = req.getParameter("secure");
+            String sigValidate = Utils.encryptSHA1(transId + "|" + amount + "|" + rescode + "|" + type + "|" + TGRConfig.gApiCheapCard.getSecret());
+            if(sig.equals(sigValidate)){
+                result = 1;
+            }
         }
 
         return result;
@@ -109,7 +173,7 @@ public class PaymentController extends HttpServlet {
         String pathInfo = req.getPathInfo() == null ? "" : req.getPathInfo();
         if (pathInfo.equals("/accept_payment")) {
             String data = req.getParameter("data");
-            String bankName = req.getParameter("bank");
+            String bankCode = req.getParameter("bank");
             String arrParam[] = data.split("\\?|\\&");
             Map mapData = new HashMap();
             for (String item : arrParam) {
@@ -120,20 +184,10 @@ public class PaymentController extends HttpServlet {
             }
 
             int bankId = 0;
-            String bankCode = "";
             long totalAmount = Long.parseLong(mapData.get("t").toString());
             String buyer = "";
             List<OrderDetail> listDetail = new ArrayList<OrderDetail>();
             String ip = Utils.getClientIP(req);
-
-            List<Bank> listBank = Utils.getListBank();
-            for (Bank item : listBank) {
-                if (item.getBankName().equals(bankName)) {
-                    bankId = item.getId();
-                    bankCode = item.getBankCode();
-                    break;
-                }
-            }
 
             if (mapData.get("acc") != null) {
                 buyer = mapData.get("acc").toString();
@@ -144,7 +198,25 @@ public class PaymentController extends HttpServlet {
                 listDetail.add(detail);
             }
             else{
-                
+                buyer = "";
+                String cardIdKey = "id0";
+                int cardIndex = 0;
+                Map<Integer,Item> mapItem = BusinessProcess.getListItem();
+                while(mapData.get(cardIdKey) != null){
+                    OrderDetail detail = new OrderDetail();
+                    String quantityKey = "q" + cardIndex;
+                    int cardId = Integer.parseInt(mapData.get(cardIdKey).toString());
+                    int quantity = Integer.parseInt(mapData.get(quantityKey).toString());
+                    int amount = (int) (mapItem.get(cardId).getUnitPrice() * ((100 - mapItem.get(cardId).getDiscountPercent()) / 100) * quantity);
+                    
+                    detail.setItemId(cardId);
+                    detail.setQuantity(quantity);
+                    detail.setAmount(amount);
+                    listDetail.add(detail);
+                    
+                    cardIndex++;
+                    cardIdKey = "id" + cardIndex;
+                }
             }
             
             String strResult = APISale.createOrder(bankId, totalAmount, "", "", "", "", listDetail, bankCode, ip, buyer);
